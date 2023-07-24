7236137f86eb2a28e73594205df981eab75a9b9f
ParkSomin23 <caqta0323@naver.com>
2023-07-25 01:51:28 +0900

    8장

---
+++ ex08/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark fixed>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-toolbar-title>마스터 페이지</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+    <v-main>
+      <!-- fluid: 좌우 여백 제거 -->
+      <v-container>
+        <h1 class="display-1 my-5">안녕하세요</h1>
+        <p class="body-2 my-4">마스터 페이지입니다</p>
+        <v-divider></v-divider>
+        <h1 class="display-3 my-5">안녕하세요</h1>
+        <p class="body-1 my-4">마스터 페이지입니다</p>
+      </v-container>
+    </v-main>
+    <v-footer color="secondary" dark fixed>
+      <div class="mx-auto">Copyright &copy;</div>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created () {
+    this.$vuetify.theme.dark = true;
+  }
+}
+</script>
---
+++ ex08/src/App2.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-main>
+      <!-- fluid: 좌우 여백 제거 -->
+      <v-container fluid fill-height>
+        <v-row>
+          <v-col cols="12" class="text-center">
+            <h1 class="title white--text">반가워요!</h1>
+            <p class="caption">by VueJS</p>
+            <img src="./assets/hello-pwa.png" alt="">
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  created () {
+    this.$vuetify.theme.dark = true;
+  }
+}
+</script>
